142a23a44522b5b63134fa0e6840cbdf92281e1b
Mickael Alliel <mickael.alliel@gmail.com>
2018-03-07 13:50:11 +0200

    refactored error handling and updatetodo

--- src/handlers/todo.js
+++ src/handlers/todo.js
@@ -20,17 +20,14 @@ exports.AddTodo = async (req, res, next) => {
 };
 
 exports.UpdateTodo = async (req, res, next) => {
-    if (!req.params.id) {
-        return res.status(400).send({message: 'Missing required parameter Todo ID'})
+    if (!req.params.id || req.body.title == null || req.body.completed == null) {
+        return res.status(400).send({message: 'Missing required parameters/payload'})
     }
     const todoId = req.params.id;
     const query = await global.Store.findById(todoId);
     let todo = query;
     if (!todo) {
         return res.status(404).send({message: 'Could not find todo'})
-    }
-    if (req.body.title == null && req.body.completed == null) {
-        return res.status(400).send({message: 'Missing required payload'})
     }
     todo.title = req.body.title;
     todo.completed = req.body.completed;
